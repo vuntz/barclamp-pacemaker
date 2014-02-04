83ba4baeef2cefe5eee63802f708e05721c9fe35
Adam Spiers <aspiers@suse.com>
2014-02-04 13:01:47 +0000

    moved shared examples for action on non-existent resource to common.rb

--- chef/cookbooks/pacemaker/spec/helpers/common.rb
+++ chef/cookbooks/pacemaker/spec/helpers/common.rb
@@ -1,5 +1,21 @@
 require 'mixlib/shellout'
 
+shared_examples "action on non-existent resource" do |action, cmd, expected_error|
+  it "should not attempt to #{action.to_s} a non-existent resource" do
+    expect_definition("")
+
+    if expected_error
+      expect { provider.run_action action }.to \
+        raise_error(RuntimeError, expected_error)
+    else
+      provider.run_action action
+    end
+
+    expect(@chef_run).not_to run_execute(cmd)
+    expect(@resource).not_to be_updated
+  end
+end
+
 module Chef::RSpec
   module Pacemaker
     module Common
